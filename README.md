4d73ed0f214b6bc2f7c5dfbe762e31d2f5e71b71
Yvain Liechti <yliechti@clever-age.com>
2019-10-04 13:16:44 +0200

    project init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# hacktoberboard
+# HacktoberBoard
+
 Board to follow Hacktoberfest participation
+
+## Contribute
+
+1. Clone your repo
+2. Install dependencies: `npm i`
+3. Create a github token
+4. Set environment variables:  
+   At first, set environment variable to be able to request on Github API
+
+   ```
+   export GITHUB_API_LOGIN="yourLogin"
+   export GITHUB_API_TOKEN="yourToken"
+   ```
+5. Start to dev `npm run start`
---
+++ src/getData.js
@@ -0,0 +1,109 @@
+
+const fetch = require('node-fetch');
+var { graphql, buildSchema } = require('graphql');
+const moment = require('moment');
+
+const start = escape(moment().month(9).startOf('month').startOf('day').format('YYYY-MM-DD'));
+const end = escape(moment().month(9).endOf('month').endOf('day').format('YYYY-MM-DD'));
+
+function btoa(str) {
+  let buffer;
+
+  if (str instanceof Buffer) {
+    buffer = str;
+  } else {
+    buffer = Buffer.from(str.toString(), 'binary');
+  }
+
+  return buffer.toString('base64');
+}
+
+function getQuery(login) {
+  return `
+    query {
+      search(query: "author:${login} -label:invalid is:pr is:public created:${start}..${end}", type: ISSUE, last: 4) {
+        issueCount
+      }
+    }
+  `
+}
+
+module.exports = async (username, token, org, verbose) => {
+  const auth = `Basic ${btoa(`${username}:${token}`)}`;
+
+  if (verbose) { console.info('Fetch org members'); }
+  const response = await fetch(`https://api.github.com/graphql`, {
+    method: 'POST',
+    headers: {
+      Authorization: auth,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      query:`
+        query {
+          organization(login:"${org}") {
+            membersWithRole(first: 100) {
+              edges {
+                node {
+                  login
+                  name
+                  avatarUrl
+                }
+              }
+            }
+          }
+        }
+      `,
+    }),
+  });
+
+
+  if (false === response.ok) {
+    return;
+  }
+  const { data : { organization : { membersWithRole : { edges } } } } = await response.json();
+
+  const membersStats = edges.map(async ({node}) => {
+    if (verbose) { console.info(`Fetch "${node.login}" pull request counter.`); }
+    try {
+      const responcePR = await fetch(`https://api.github.com/graphql`, {
+        method: 'POST',
+        headers: {
+          Authorization: auth,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          query: getQuery(node.login),
+        }),
+      });
+
+      if (false === responcePR.ok) {
+        console.log(`response KO for "${node.login}"`, responcePR);
+        node.prCount = null;
+        return node;
+      }
+
+      const { data : { search : {issueCount} } } = await responcePR.json();
+
+      node.prCount = issueCount;
+      return node;
+
+    } catch (err) {
+      console.error(err.message)
+    }
+
+    node.prCount = null;
+
+    return node;
+  });
+
+  const result = await Promise.all(membersStats);
+
+  return result.sort((a, b) => {
+    if (a.prCount === b.prCount) { return 0; }
+    if (a.prCount > b.prCount || b.prCount === null) { return -1; }
+    return 1;
+  })
+};
